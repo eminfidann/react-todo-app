5ccb6a29d9b18fe07b478ddab1fd3caa07733649
Emin Fidan <fidanemin42.ef@gmail.com>
2022-06-27 15:24:59 +0300

    todo list cleared

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,7 @@ import React, { useState } from "react";
 import { TodoList } from "./TodoList";
 import { AddTodoForm } from "./AddTodoForm";
 
-const initialTodos: Todo[] = [
-  {
-    text: "Walk the dog",
-    complete: false,
-  },
-  {
-    text: "Write app",
-    complete: true,
-  },
-];
+const initialTodos: Todo[] = [];
 
 function App() {
   const [todos, setTodos] = useState(initialTodos);
